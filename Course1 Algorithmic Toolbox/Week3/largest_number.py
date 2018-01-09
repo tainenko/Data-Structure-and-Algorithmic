bab63f0687dbbac41282a71cf2c1822f53bd8ccf
tainenko <31752048+tainenko@users.noreply.github.com>
2018-01-09 19:16:05 +0800

    Week4 original assignments

--- Course1 Algorithmic Toolbox/Week3/largest_number.py
+++ Course1 Algorithmic Toolbox/Week3/largest_number.py
@@ -1,16 +1,21 @@
 #Uses python3
 
 import sys
+from functools import cmp_to_key
 
 def largest_number(a):
     #write your code here
     res=""
+    a.sort(key=cmp_to_key(lambda x,y: int(y+x)-int(x+y)))
+    return '0' if a[0] == '0' else ''.join(x for x in a)
+    '''
     m = len(max(a, key=len))
     newlist=[(i,i+i[-1]*(m-len(i)))for i in a]
     newlist.sort(key=lambda k:k[1], reverse=True)
 
     res=''.join((k[0] for k in newlist))
     return res
+    '''
 
 
 if __name__ == '__main__':
