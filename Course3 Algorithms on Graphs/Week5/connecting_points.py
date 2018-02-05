8f792c9c903ed4f5c94e0aade1a046e714623c88
tainenko <31752048+tainenko@users.noreply.github.com>
2018-02-05 09:28:27 +0800

    Update week5 implement

--- Course3 Algorithms on Graphs/Week5/connecting_points.py
+++ Course3 Algorithms on Graphs/Week5/connecting_points.py
@@ -2,9 +2,32 @@
 import sys
 import math
 
-def minimum_distance(x, y):
+def length(xi, yi, xj, yj):
+    return math.sqrt(math.pow(xi-xj, 2) + math.pow(yi-yj, 2))
+
+def minimum_distance(n,x,y):
     result = 0.
-    #write your code here
+    adj = [[] for _ in range(n)]
+    weight = [[0] * n for _ in range(n)]
+    for i in range(n):
+        adj[i] = list(v for v in range(n) if v != i)
+        for j in range(n):
+            if i != j:
+                weight[i][j] = length(x[i], y[i], x[j], y[j])
+                weight[j][i] = length(x[i], y[i], x[j], y[j])
+    X = set()
+    X.add(0)
+
+    while len(X) != n:
+        crossing = set()
+        for u in X:
+            for v in adj[u]:
+                if v not in X:
+                    crossing.add((u, v))
+        edge = sorted(crossing, key=lambda e: weight[e[0]][e[1]])[0]
+        result += weight[edge[0]][edge[1]]
+        X.add(edge[1])
+
     return result
 
 
@@ -14,4 +37,4 @@ if __name__ == '__main__':
     n = data[0]
     x = data[1::2]
     y = data[2::2]
-    print("{0:.9f}".format(minimum_distance(x, y)))
+    print("{0:.9f}".format(minimum_distance(n,x, y)))
